fefbb36c76841e8de2bbeb273c6628ee384258c2
Your Name <you@example.com>
2022-10-27 19:15:38 +0300

    finished pattern builder

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
+    Builder builder = new Builder.MyBuilder().calories(100).minerals(15).fat(10_000).protein(40).build();
+        System.out.println(builder.calories);
     }
 }
