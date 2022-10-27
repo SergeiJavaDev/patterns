e7b915e389e6ccd12c785332d37c2426b90079e5
Your Name <you@example.com>
2022-10-27 19:06:43 +0300

    create nested class MyBuilder

---
+++ src/com/company/Builder.java
@@ -0,0 +1,44 @@
+package com.company;
+
+public class Builder {
+
+    int calories;
+    int minerals;
+    int fat;
+    int protein;
+
+static class MyBuilder{
+
+    int calories = 100;
+    int minerals = 20;
+    int fat = 15;
+    int protein = 30;
+
+    public MyBuilder calories(int value){
+
+        calories = value;
+        return this;
+    }
+
+    public MyBuilder fat(int value){
+
+        fat = value;
+        return this;
+    }
+
+    public MyBuilder minerals(int value){
+
+        minerals = value;
+        return this;
+    }
+
+    public MyBuilder protein(int value){
+
+        protein = value;
+        return this;
+    }
+}
+
+
+}
+
